958b7a52a55ac7483865205d0a5435843242f1ab
clix29 <clixx29@gmail.com>
2024-08-05 23:12:39 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,19 @@
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MyFirstFrame myFrame; 
+        
+        myFrame = new MyFirstFrame();
+        
+        
+        
+    }
+    
+}
